03b8367794237b2dee2c405efcd0d4f9a55e3a74
zmmfly <shenqhy@qq.com>
2021-12-22 15:21:39 +0800

    move RV32I_instruction place.

--- xmake.lua
+++ xmake.lua
@@ -2,7 +2,7 @@ includes("libs")
 
 target("riscv-vm")
     set_kind("binary")
-    add_deps("xutils", "xvvm")
+    add_deps("xutils", "rvvm")
     add_includedirs("include")
     add_files("src/*.cc")
     add_files("src/ins/*.cc")
