36d28b70d59163da7474b8daac1455eb638d754f
Zmmfly <shenqhy@qq.com>
2024-01-21 23:35:45 +0800

    improve test build script

--- tests/xmake.lua
+++ tests/xmake.lua
@@ -1,14 +1,22 @@
-add_requires("gtest")
+add_requires("gtest", "fmt")
+
+target("test_rv32_common")
+    set_kind("static")
+    set_languages("c++20")
+    add_deps("rvvm")
+
+    add_files("src/tester_rv32i.cc")
+    add_includedirs("inc", {public=true})
+
 for _, file in ipairs(os.files("src/rv32/test_*.cc")) do
     local name = path.basename(file)
     target(name)
         set_kind("binary")
         set_default(false)
         set_languages("c++20")
-        add_deps("rvvm")
-        add_files("src/tester_rv32i.cc")
+        add_deps("rvvm", "test_rv32_common")
         add_files("src/rv32/" .. name .. ".cc")
         add_includedirs("inc")
         add_tests("default")
-        add_packages("gtest")
+        add_packages("gtest", "fmt")
 end
