d77405ad4a9ad9a3c3a0560414ef607d4120114a
Zmmfly <shenqhy@qq.com>
2024-01-23 00:27:42 +0800

    implement test case for RV32I JALR

--- tests/src/rv32/test_RV32I_JALR.cc
+++ tests/src/rv32/test_RV32I_JALR.cc
@@ -1,10 +1,40 @@
 #include <cstdio>
 #include <iostream>
 #include "zmmfly/rv32I.h"
+#include "tester_rv32i.h"
 #include <gtest/gtest.h>
+#include <fmt/core.h>
+
+tester_rv32i tester;
+zmmfly::rv32::inst_type inst;
 
 int main(int argc, char** argv)
 {
+    inst.opcode = 0b1100111;
+    inst.others = 0;
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
+}
+
+TEST(JALR, JALR)
+{
+    tester.reset();
+    tester.m_mem->reset();
+
+    auto& regs = tester.regs;
+    auto& test = tester;
+    auto  mem  = tester.m_mem;
+
+    inst.others        = 0;
+    inst.I.func3       = 0b000;
+    inst.I.rd          = 1;
+    inst.I.rs1         = 2;
+    inst.I.imm         = -5;
+    regs.pc            = 12;
+    regs.x[inst.I.rs1] = 12;
+
+    auto res1 = test.exec(inst.u32);
+    EXPECT_EQ(RV_EOK, res1);
+    EXPECT_EQ(16, regs.x[inst.R.rd]);
+    EXPECT_EQ(6, regs.pc);
 }
