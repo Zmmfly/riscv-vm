619cc6d18416df7046b259f12c19374fc74abb0c
Zmmfly <shenqhy@qq.com>
2024-01-24 00:51:28 +0800

    implement test case for RV32I SYSTEM

--- tests/src/rv32/test_RV32I_SYSTEM.cc
+++ tests/src/rv32/test_RV32I_SYSTEM.cc
@@ -1,10 +1,69 @@
 #include <cstdio>
 #include <iostream>
 #include "zmmfly/rv32I.h"
+#include "tester_rv32i.h"
 #include <gtest/gtest.h>
+#include <fmt/core.h>
+
+tester_rv32i tester;
+zmmfly::rv32::inst_type inst;
 
 int main(int argc, char** argv)
 {
+    inst.opcode = 0b1110011;
+    inst.others = 0;
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
+}
+
+TEST(SYSTEM, ECALL)
+{
+    tester.reset();
+    tester.m_mem->reset();
+
+    auto& regs = tester.regs;
+    auto& test = tester;
+    auto  mem  = tester.m_mem;
+
+    inst.others  = 0;
+    inst.I.rd    = 0;
+    inst.I.func3 = 0;
+    inst.I.rs1   = 0;
+    inst.I.imm   = 0;
+
+    bool called = false;
+
+    test.instmap["I"]->set("ecall", zmmfly::rv32::v1::I::ext_call_t([&called](zmmfly::rv32::inst_type inst){
+        called = true;
+    }));
+
+    auto res1 = test.exec(inst.u32);
+    EXPECT_EQ(RV_EOK, res1);
+    EXPECT_EQ(true, called);
+}
+
+TEST(SYSTEM, EBREAK)
+{
+    tester.reset();
+    tester.m_mem->reset();
+
+    auto& regs = tester.regs;
+    auto& test = tester;
+    auto  mem  = tester.m_mem;
+
+    inst.others  = 0;
+    inst.I.rd    = 0;
+    inst.I.func3 = 0;
+    inst.I.rs1   = 0;
+    inst.I.imm   = 1;
+
+    bool called = false;
+
+    test.instmap["I"]->set("ebreak", zmmfly::rv32::v1::I::ext_call_t([&called](zmmfly::rv32::inst_type inst){
+        called = true;
+    }));
+
+    auto res1 = test.exec(inst.u32);
+    EXPECT_EQ(RV_EOK, res1);
+    EXPECT_EQ(true, called);
 }
