7778e7d861de41faa0242c69cf01816da44cf4b0
Zmmfly <shenqhy@qq.com>
2024-01-25 01:27:52 +0800

    add IN005

--- inc/zmmfly/rv32M.h
+++ inc/zmmfly/rv32M.h
@@ -2,8 +2,9 @@
 #define __ZMMFLY_RV32_M_H__
 
 #include "zmmfly/rv32_base.h"
+#include <functional>
 
-namespace zmmfly::rv32
+namespace zmmfly::rv32::v1
 {
 
 class M
@@ -11,9 +12,18 @@ class M
 {
 
 public:
-    M() {};
+    using ext_call_t = std::function<void(rv32::inst_type)>;
 
-    rv_err_t execute_normal(uint32_t inst, registers_t& regs, bus_t& bus);
+    M();
+
+    std::string name();
+
+    rv_err_t set(std::string k, std::any v);
+
+    rv_err_t execute_normal(uint32_t inst, registers_t& regs, bus_t& bus, inst_map_t& inst_map);
+
+private:
+    ext_call_t m_dvi_zero;
 };
 
 };
