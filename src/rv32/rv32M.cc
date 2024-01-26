f30a5b9f7213f5c22aca21441908388541a7329b
Zmmfly <shenqhy@qq.com>
2024-01-26 22:46:10 +0800

    implement RV32M extension instruction set

--- src/rv32/rv32M.cc
+++ src/rv32/rv32M.cc
@@ -14,26 +14,69 @@ rv_err_t M::execute_normal(uint32_t inst, registers_t& regs, bus_t& bus, inst_ma
         case 0b01'100'11: {
             if (iref.func3 == 0b000) { // mul
                 // x[rd] = (s32) x[rs1] * (s32) x[rs2]
+                regs.x[iref.R.rd] = (int32_t)regs.x[iref.R.rs1] * (int32_t)regs.x[iref.R.rs2];
+                res = RV_EOK;
+
             } else if (iref.func3 == 0b001) { // mulh
                 // x[rd] = ((s64) x[rs1] * (s64) x[rs2]) >> XLEN
+                regs.x[iref.R.rd] = ( (int64_t)regs.x[iref.R.rs1] * (int64_t)regs.x[iref.R.rs2] ) >> 32;
+                res = RV_EOK;
+
             } else if (iref.func3 == 0b010) { // mulhsu
                 // x[rd] = ((s64) x[rs1] * (u64) x[rs2]) >> XLEN
+                regs.x[iref.R.rd] = ( (int64_t)regs.x[iref.R.rs1] * (uint64_t)regs.x[iref.R.rs2] ) >> 32;
+                res = RV_EOK;
+
             } else if (iref.func3 == 0b011) { // mulhu
                 // x[rd] = ((u64) x[rs1] * (u64) x[rs2]) >> XLEN
+                regs.x[iref.R.rd] = ( (uint64_t)regs.x[iref.R.rs1] * (uint64_t)regs.x[iref.R.rs2] ) >> 32;
+                res = RV_EOK;
+
             } else if (iref.func3 == 0b100) { // div
                 // x[rd] = (s32) x[rs1] / (s32) x[rs2]
+                if (regs.x[iref.R.rs2] == 0) {
+                    regs.x[iref.R.rd] = 0xffffffff;
+                } else {
+                    regs.x[iref.R.rd] = (int32_t)regs.x[iref.R.rs1] / (int32_t)regs.x[iref.R.rs2];
+                }
+                res = RV_EOK;
+
             } else if (iref.func3 == 0b101) { // divu
                 // x[rd] = (u32) x[rs1] / (u32) x[rs2]
+                if (regs.x[iref.R.rs2] == 0) {
+                    regs.x[iref.R.rd] = 0xffffffff;
+                } else {
+                    regs.x[iref.R.rd] = regs.x[iref.R.rs1] / regs.x[iref.R.rs2];
+                }
+                res = RV_EOK;
+
             } else if (iref.func3 == 0b110) { // rem
                 // x[rd] = (s32) x[rs1] % (s32) x[rs2]
+                if (regs.x[iref.R.rs2] == 0) { 
+                    regs.x[iref.R.rd] = regs.x[iref.R.rs1];
+                } else {
+                    regs.x[iref.R.rd] = (int32_t)regs.x[iref.R.rs1] % (int32_t)regs.x[iref.R.rs2];
+                }
+                res = RV_EOK;
+
             } else if (iref.func3 == 0b111) { // remu
                 // x[rd] = (u32) x[rs1] % (u32) x[rs2]
+                if (regs.x[iref.R.rs2] == 0) { 
+                    regs.x[iref.R.rd] = regs.x[iref.R.rs1];
+                } else {
+                    regs.x[iref.R.rd] = (uint32_t)regs.x[iref.R.rs1] % (uint32_t)regs.x[iref.R.rs2];
+                }
+                res = RV_EOK;
+
+            } else {
+                break;
             }
             break;
         }
         default:
             break;
     }
+    regs.x[0] = 0;
     return res;
 }
 
