e4d459207b8e76c55e1d0566938ab70a10169123
Zmmfly <shenqhy@qq.com>
2024-01-30 22:48:48 +0800

    add more common include for rv32.h

--- inc/zmmfly/rv32.h
+++ inc/zmmfly/rv32.h
@@ -2,5 +2,9 @@
 #define __ZMMFLY_RVVM_RV32_H__
 
 #include "zmmfly/rv32I.h"
+#include "zmmfly/rv32M.h"
+#include "zmmfly/rv32A.h"
+#include "zmmfly/rv32F.h"
+#include "zmmfly/rv32D.h"
 
 #endif /* __ZMMFLY_RVVM_RV32_H__ */
