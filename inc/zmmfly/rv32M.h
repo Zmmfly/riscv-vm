0c2801465698a3effcb83e283fa296ac63e6a2d0
Zmmfly <shenqhy@qq.com>
2024-02-02 20:12:17 +0800

    del unused code

--- inc/zmmfly/rv32M.h
+++ inc/zmmfly/rv32M.h
@@ -12,16 +12,11 @@ class M
 {
 
 public:
-    using ext_call_t = std::function<void(rv32::inst_type)>;
-
     M();
 
     std::string name();
 
     rv_err_t execute_normal(uint32_t inst, registers_t& regs, bus_t& bus, inst_map_t& inst_map);
-
-private:
-    ext_call_t m_dvi_zero;
 };
 
 };
