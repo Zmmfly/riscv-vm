64615b8dfc8a392f20a6c793656e4a483ea0741d
Zmmfly <shenqhy@qq.com>
2024-02-21 23:38:40 +0800

    change A extension implement method from if to switch

--- src/rv32/rv32A.cc
+++ src/rv32/rv32A.cc
@@ -17,41 +17,43 @@ rv_err_t A::execute_normal(uint32_t inst, registers_t& regs, bus_t& bus, inst_ma
     regs.x[0] = 0;
     switch(iref.opcode){
         case 0b01'011'11: {
-            if (iref.R_amo.func3 != 0b010) break;
-            
-            if (iref.R_amo.func5 == 0b000'10) { /* lr.w */
+            switch(iref.R_amo.func5) {
+                case 0b000'10:  /* lr.w */ {
+                    break;
+                }
+                case 0b000'11:  /* sc.w */ {
+                    break;
+                }
+                case 0b000'01:  /* amoswap.w */ {
+                    break;
+                }
+                case 0b000'00:  /* amoadd.w */ {
+                    break;
+                }
+                case 0b001'00:  /* amoxor.w */ {
+                    break;
+                }
+                case 0b011'00:  /* amoand.w */ {
+                    break;
+                }
+                case 0b010'00:  /* amoor.w */ {
+                    break;
+                }
+                case 0b100'00:  /* amomin.w */ {
+                    break;
+                }
+                case 0b101'00:  /* amomax.w */ {
+                    break;
+                }
+                case 0b110'00:  /* amominu.w */ {
+                    break;
+                }
+                case 0b111'00:  /* amomaxu.w */ {
+                    break;
+                }
+                default:
+                    break;
             }
-
-            else if (iref.R_amo.func5 == 0b000'11) {    /* sc.w */
-            }
-
-            else if (iref.R_amo.func5 == 0b000'01) {    /* amoswap.w */
-            }
-
-            else if (iref.R_amo.func5 == 0b000'00) {    /* amoadd.w */
-            }
-
-            else if (iref.R_amo.func5 == 0b001'00) {    /* amoxor.w */
-            }
-
-            else if (iref.R_amo.func5 == 0b011'00) {    /* amoand.w */
-            }
-
-            else if (iref.R_amo.func5 == 0b010'00) {    /* amoor.w */
-            }
-
-            else if (iref.R_amo.func5 == 0b100'00) {    /* amomin.w */
-            }
-
-            else if (iref.R_amo.func5 == 0b101'00) {    /* amomax.w */
-            }
-
-            else if (iref.R_amo.func5 == 0b110'00) {    /* amominu.w */
-            }
-
-            else if (iref.R_amo.func5 == 0b111'00) {    /* amomaxu.w */
-            }
-
             break;
         }
         default:
